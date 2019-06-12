ccfda74d0e7d49e150e06d0fd6165042ac468d30
ruucm <ruucm.a@gmail.com>
2019-06-12 14:55:57 +0900

    add meta tags

--- pages/index.js
+++ pages/index.js
@@ -7,18 +7,34 @@ export default withData(props => (
   <App>
     <Helmet>
       <title>index</title>
-      <meta name="description" content="Helmet application" />
+      {/* <meta name="description" content="Helmet application" /> */}
       <meta property="og:type" content="article" />
-      <meta
+      {/* <meta
         property="og:image"
         content="https://yt3.ggpht.com/a/AGF-l79odxmUaLdPO8rxtYP3un8kS7p3aJnUGmKJGg=s900-mo-c-c0xffffffff-rj-k-no"
-      />
+      /> */}
     </Helmet>
     <div>
       <h1>index</h1>
       <p>HELLO WORLD! HELLO FROM RAN!</p>
+      <p>HELLO WORLD! HELLO FROM RAN!</p>
+      <p>HELLO WORLD! HELLO FROM RAN!</p>
+      <p>HELLO WORLD! HELLO FROM RAN!</p>
+      <p>
+        HELLO WORLD! HELLO FROM RALLO WORLD! HELLO FROM RALLO WORLD! HELLO FROM
+        RALLO WORLD! HELLO FROM RALLO WORLD! HELLO FROM RALLO WORLD! HELLO FROM
+        RALLO WORLD! HELLO FROM RALLO WORLD! HELLO FROM RALLO WORLD! HELLO FROM
+        RALLO WORLD! HELLO FROM RALLO WORLD! HELLO FROM RALLO WORLD! HELLO FROM
+        RALLO WORLD! HELLO FROM RALLO WORLD! HELLO FROM RALLO WORLD! HELLO FROM
+        RALLO WORLD! HELLO FROM RALLO WORLD! HELLO FROM RALLO WORLD! HELLO FROM
+        RAN!
+      </p>
       <hr />
       <pre>{dump(props)}</pre>
+      <img
+        src="https://fsmedia.imgix.net/07/05/10/11/b2e4/4575/937c/7b9a134d6ae4/5pikachu-1gif.gif?rect=0%2C0%2C1000%2C500&auto=compress&dpr=2&w=650&fm=jpg"
+        alt="pika"
+      />
     </div>
   </App>
 ));
